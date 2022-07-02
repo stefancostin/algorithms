7e9f94a45b3a4cc7139209d4929158786113bf3c
Stefan Costin <stefanfcostin@gmail.com>
2022-07-02 10:35:32 +0300

    Format files

--- udemy/src/io/github/stefancostin/udemy/algorithms/BubbleSort.java
+++ udemy/src/io/github/stefancostin/udemy/algorithms/BubbleSort.java
@@ -1,5 +1,7 @@
 package io.github.stefancostin.udemy.algorithms;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
 	public static void main(String[] args) {
@@ -7,27 +9,22 @@ public class BubbleSort {
 		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
 			for (int i = 0; i < lastUnsortedIndex; i++) {
 				if (intArray[i] > intArray[i + 1]) {
-					swap (intArray, i, i + 1);
+					swap(intArray, i, i + 1);
 				}
 			}
 		}
-		print(intArray);
+		
+		System.out.println(Arrays.toString(intArray));
 	}
-	
+
 	public static void swap(int[] arr, int firstIndex, int secondIndex) {
 		if (firstIndex == secondIndex) {
 			return;
 		}
+		
 		int temp = arr[firstIndex];
 		arr[firstIndex] = arr[secondIndex];
 		arr[secondIndex] = temp;
-	}
-	
-	public static void print(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + "  ");
-		}
-		System.out.print("\n");
 	}
 
 }
--- udemy/src/io/github/stefancostin/udemy/algorithms/SelectionSort.java
+++ udemy/src/io/github/stefancostin/udemy/algorithms/SelectionSort.java
@@ -1,36 +1,35 @@
 package io.github.stefancostin.udemy.algorithms;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
 	public static void main(String[] args) {
 		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
-		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
-				lastUnsortedIndex--) {
+		
+		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
 			int largest = 0;
+			
 			for (int i = 0; i <= lastUnsortedIndex; i++) {
 				if (intArray[i] >= intArray[largest]) {
 					largest = i;
 				}
 			}
-			swap (intArray, largest, lastUnsortedIndex);
+			
+			swap(intArray, largest, lastUnsortedIndex);
 		}
-		print(intArray);
+		
+		System.out.println(Arrays.toString(intArray));
 	}
-	
+
 	public static void swap(int[] arr, int firstIndex, int secondIndex) {
 		if (firstIndex == secondIndex) {
 			return;
 		}
+		
 		int temp = arr[firstIndex];
 		arr[firstIndex] = arr[secondIndex];
 		arr[secondIndex] = temp;
 	}
 	
-	public static void print(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + "  ");
-		}
-		System.out.print("\n");
-	}
-
 }
